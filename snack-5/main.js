f370179cf26cf2778d9c6f9a13503d42f79f27c5
catanielena <catani.elena1@gmail.com>
2021-08-27 16:34:14 +0200

    Refact: for to while

--- snack-5/main.js
+++ snack-5/main.js
@@ -17,17 +17,21 @@ for (var i =0; i< Math.floor(Math.random()*50); i++) {
 console.log(elm1);
 
 if (elm1.length < elm2.length) {
-    for (i = elm1.length; i < elm2.length; i++) {
+    i = elm1.length;
+    while (i < elm2.length) {
         var newNum = Math.floor(Math.random()*10);
-        console.log("numero nuovo: " + newNum);
+        console.log("new number: " + newNum);
         elm1.push(newNum);
+        i++;
     }
-    console.log(elm1)
+    console.log(elm1);
 } else if (elm2.length < elm1.length) {
-    for (i = elm2.length; i < elm1.length; i++) {
+    i = elm2.length;
+    while (i < elm1.length) {
         var newNum = Math.floor(Math.random()*10);
-        console.log("numero nuovo: " + newNum);
+        console.log("new number: " + newNum);
         elm2.push(newNum);
+        i++;
     }
     console.log(elm2);
 } else {
