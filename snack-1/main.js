efcd32afa0d7bb069d557f34d7a76dd84fc5e883
catanielena <catani.elena1@gmail.com>
2021-08-27 11:48:36 +0200

    Add: snack-1

---
+++ snack-1/main.js
@@ -0,0 +1,31 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+// versione for
+var somma = 0;
+for (var i=0; i<5; i++) {
+    var num = parseInt(prompt("Inserisci un numero"));
+    while (isNaN(num)) {
+       var numero = parseInt(prompt("errore! inserisci un numero"));
+    }
+    somma += num;
+}
+console.log(somma);
+
+// // versione while
+var somma = 0;
+var i=0;
+while (i<5) {
+    do {
+        var num = parseInt(prompt("Inserisci un numero"));
+        if(isNaN(num)) {
+            alert("Errore inserisci un numero!");
+        }
+    } while (isNaN(num));
+    somma += num;
+    i++;
+}
+console.log(somma);
+
+
