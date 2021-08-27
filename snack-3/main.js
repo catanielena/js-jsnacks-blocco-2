ea641ad832bfd8b276400897121b573841504f3a
catanielena <catani.elena1@gmail.com>
2021-08-27 13:00:08 +0200

    Fix: listaInvitati-snack-3

--- snack-3/main.js
+++ snack-3/main.js
@@ -5,7 +5,7 @@ var nome = ["Gianni", "Maddalena", "Elisa", "Giovanni", "Maria"];
 var cognome = ["Bianchini", "Modonesi", "Scaroni", "Milanese", "Giannotti"];
 var listaInvitati =[];
 for (var i=0; i<3; i++) {
-    listaInvitati.push(nome[Math.floor(Math.random() * 5)] + " " + cognome[Math.floor(Math.random() * 5)]);
+    listaInvitati.push(nome[Math.floor(Math.random() * nome.length)] + " " + cognome[Math.floor(Math.random() * cognome-length)]);
 }
 
 console.log(listaInvitati);
