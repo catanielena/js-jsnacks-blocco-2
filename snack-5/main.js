ed783b8af39ca13313fcff0c1a171018871fbde7
catanielena <catani.elena1@gmail.com>
2021-08-27 16:30:40 +0200

    Add: random-list

--- snack-5/main.js
+++ snack-5/main.js
@@ -2,8 +2,19 @@
 // Aggiungi elementi casuali all’array che ha meno elementi,
 // fino a quando ne avrà tanti quanti l’altro.
 
-var elm1 = [2, 3, 6, 4, 3, 6];
-var elm2 = [6, 7];
+var elm1 = [];
+for (var i =0; i< Math.floor(Math.random()*50); i++) {
+    var elm1Element = Math.floor(Math.random()*10);
+    elm1.push(elm1Element);
+}
+console.log(elm1);
+
+var elm2 = [];
+for (var i =0; i< Math.floor(Math.random()*50); i++) {
+    var elm2Element = Math.floor(Math.random()*10);
+    elm1.push(elm2Element);
+}
+console.log(elm1);
 
 if (elm1.length < elm2.length) {
     for (i = elm1.length; i < elm2.length; i++) {
@@ -12,11 +23,13 @@ if (elm1.length < elm2.length) {
         elm1.push(newNum);
     }
     console.log(elm1)
-} else {
+} else if (elm2.length < elm1.length) {
     for (i = elm2.length; i < elm1.length; i++) {
         var newNum = Math.floor(Math.random()*10);
         console.log("numero nuovo: " + newNum);
         elm2.push(newNum);
     }
     console.log(elm2);
+} else {
+    console.log("Le due liste hanno lo stesso numero di elementi");
 }
