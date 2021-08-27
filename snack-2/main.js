3f1da5ec75fb79c42280adb948b2506ff09115f6
catanielena <catani.elena1@gmail.com>
2021-08-27 12:18:31 +0200

    Add: snack-2

---
+++ snack-2/main.js
@@ -0,0 +1,13 @@
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+var num = parseInt(prompt("Inserisci un numero"));
+while (isNaN(num)) {
+    var num = parseInt(prompt("Errore! Inserisci un numero"));
+}
+
+if( (num % 2) == 0) {
+    console.log(num);
+} else {
+    console.log(num + 1)
+}
