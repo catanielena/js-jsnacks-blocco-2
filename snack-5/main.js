cc2a685afd65353884be154c491a20a22ac266fb
catanielena <catani.elena1@gmail.com>
2021-08-30 10:19:35 +0200

    Refact: snack-5__short-solution

--- snack-5/main.js
+++ snack-5/main.js
@@ -4,36 +4,47 @@
 
 var elm1 = [];
 for (var i =0; i< Math.floor(Math.random()*50); i++) {
-    var elm1Element = Math.floor(Math.random()*10);
-    elm1.push(elm1Element);
+    elm1.push(Math.floor(Math.random()*10));
 }
 console.log(elm1);
 
 var elm2 = [];
 for (var i =0; i< Math.floor(Math.random()*50); i++) {
-    var elm2Element = Math.floor(Math.random()*10);
-    elm1.push(elm2Element);
+    elm1.push(Math.floor(Math.random()*10));
 }
 console.log(elm1);
 
-if (elm1.length < elm2.length) {
-    i = elm1.length;
-    while (i < elm2.length) {
-        var newNum = Math.floor(Math.random()*10);
-        console.log("new number: " + newNum);
-        elm1.push(newNum);
-        i++;
-    }
-    console.log(elm1);
-} else if (elm2.length < elm1.length) {
-    i = elm2.length;
-    while (i < elm1.length) {
-        var newNum = Math.floor(Math.random()*10);
-        console.log("new number: " + newNum);
-        elm2.push(newNum);
-        i++;
-    }
-    console.log(elm2);
-} else {
+// if (elm1.length < elm2.length) {
+//     i = elm1.length;
+//     while (i < elm2.length) {
+//         var newNum = Math.floor(Math.random()*10);
+//         console.log("new number: " + newNum);
+//         elm1.push(newNum);
+//         i++;
+//     }
+//     console.log(elm1);
+// } else if (elm2.length < elm1.length) {
+//     i = elm2.length;
+//     while (i < elm1.length) {
+//         var newNum = Math.floor(Math.random()*10);
+//         console.log("new number: " + newNum);
+//         elm2.push(newNum);
+//         i++;
+//     }
+//     console.log(elm2);
+// } else {
+//     console.log("Le due liste hanno lo stesso numero di elementi");
+// }
+
+// Soluzione ottimizzata
+if (elm1.length == elm2.length) {
     console.log("Le due liste hanno lo stesso numero di elementi");
+} else {
+        while (elm1.length < elm2.length) {
+            elm1.push(Math.floor(Math.random()*10));
+        }
+
+        while (elm2.length < elm1.length) {
+        elm2.push(Math.floor(Math.random()*10));
+    }
 }
